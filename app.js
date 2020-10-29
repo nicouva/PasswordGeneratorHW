7c61cb061d65273cd6711023dfb07d4f3aa20e1a
Nicole Uva <nicouva@gmail.com>
2020-10-28 22:36:21 -0400

    declaring values

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let lowerCase = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let specialChar = ['!', '@', '#', '$', '%', '&']
+let num = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
