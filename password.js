c4761d5d812d2faf04c6e279efcdcb64a666f0f2
Nicole Uva <nicouva@gmail.com>
2020-11-02 12:08:43 -0500

    updating code

--- password.js
+++ password.js
@@ -1,47 +1,50 @@
+//Assigning variable code
+const generateBtn = document.querySelector('#generate');
+
 //assigning value to arrays
+for generatePassword() => {
+  let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  let specialChar = ['!', '@', '#', '$', '%', '&']
+  let num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-const specialChar = ['!', '@', '#', '$', '%', '&']
-const num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+  alert('Password Expired. Please create a new password with the following characters.')
 
-alert('Password Expired. Please create a new password with the following characters.')
+  //creating a collection of arrays in order to push certain arrays
 
-//creating a collection of arrays in order to push certain arrays
+  let availableCollection = [upperCase, lowerCase, specialChar, num]
 
-let availableCollection = [upperCase, lowerCase, specialChar, num]
+  let length = parseInt(prompt('Please choose the length of your new password between 8 - 128 characters.'))
 
-//write password to #password input
+  //write password to #password input
 
-function writePassword() {
-  let upperCollection = prompt('Please select a capitalized letter.')
-  if (upperCollection === 'OK') {
-    availableCollection.push(upperCase)
-  }
+  if (length > 7 && length < 129) {
 
-  let lowerCollection = prompt('Please select a lowercase letter.')
-  if (lowerCollection === 'OK') {
-    availableCollection.push(lowerCase)
-  }
+    let newPassword = ''
+    let newPasswordCharacter = ''
 
-  let specCollection = prompt('Please select a special character.')
-  if (specCollection === 'OK') {
-    availableCollection.push(specialChar)
-  }
+    let upperCollection = prompt('Please select a capitalized letter.')
+    if (upperCollection === 'OK') {
+      availableCollection.push(upperCase)
 
-  let numCollection = prompt('Please select a number from 0 - 9.')
-  if (numCollection === 'OK') {
-    availableCollection.push(num)
-  }
-}
+      let lowerCollection = prompt('Please select a lowercase letter.')
+      if (lowerCollection === 'OK') {
+        availableCollection.push(lowerCase)
+      }
 
-function length() {
-  for (i = 0; i < 10; i++) {
-    let passLength = prompt('Please determine password length.')
-    let count = availableCollection[Math.floor(Math.random) * availableCollection.length]
-  }
-}
+      let specCollection = prompt('Please select a special character.')
+      if (specCollection === 'OK') {
+        availableCollection.push(specialChar)
+      }
 
-document.addEventListener("click", writePassword); {
-  document.getElementById('generate').textContent = writePassword
-}
+      let numCollection = prompt('Please select a number from 0 - 9.')
+      if (numCollection === 'OK') {
+        availableCollection.push(num)
+      }
+
+    }
+
+
+    document.addEventListener("click", writePassword); {
+      document.getElementById('generate').textContent = writePassword
+    }
